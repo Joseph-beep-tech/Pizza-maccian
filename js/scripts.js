d39c6674f60cc3075a5b598d609c2e26c3aac701
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-10-17 22:59:28 +0300

    debbugged codes in js file

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,28 @@ $(document).ready(function() {
     $(".j2").hover(function() {
         $("#m2").toggle();
     })
+    
     $(".j3").hover(function() {
         $("#m3").toggle();
     });
+
     $(".j4").hover(function() {
         $("#m4").toggle();
+    });
+
+    $(".j5").hover(function() {
+        $("#m5").toggle();
+    });
+
+    $(".j6").hover(function() {
+        $("#m6").toggle();
+    });
+
+    $(".j7").hover(function() {
+        $("#m7").toggle();
+    });
+
+    $(".j8").hover(function() {
+        $("#m8").toggle();
     });
 });
