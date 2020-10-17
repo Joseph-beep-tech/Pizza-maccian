4d94d463eaab8740654356ad3bf8d4c042a7eeb4
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-10-17 13:05:49 +0300

    recent commits

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,14 @@ $(document).ready(function() {
     $(".j1").hover(function() {
         $("#m1").toggle();
     });
-    
+
     $(".j2").hover(function() {
         $("#m2").toggle();
     })
+    $(".j3").hover(function() {
+        $("#m3").toggle();
+    });
+    $(".j4").hover(function() {
+        $("#m4").toggle();
+    });
 });
